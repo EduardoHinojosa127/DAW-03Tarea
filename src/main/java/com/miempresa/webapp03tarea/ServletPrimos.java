788bf21c085d99d1aafb89a8fdcb03625f143340
EduardoHinojosa127 <eduardo.hinojosa@tecsup.edu.pe>
2022-09-14 21:27:41 -0500

    DAW-03Tarea

---
+++ src/main/java/com/miempresa/webapp03tarea/ServletPrimos.java
@@ -0,0 +1,116 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package com.miempresa.webapp03tarea;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Usuario
+ */
+@WebServlet(name = "ServletPrimos", urlPatterns = {"/ServletPrimos"})
+public class ServletPrimos extends HttpServlet {
+    public static boolean esPrimo(int numero) {
+        // El 0, 1 y 4 no son primos
+        if (numero == 0 || numero == 1 || numero == 4) {
+          return false;
+        }
+        for (int x = 2; x < numero / 2; x++) {
+          // Si es divisible por cualquiera de estos números, no
+          // es primo
+          if (numero % x == 0){
+             return false; 
+          }
+            
+        }
+        // Si no se pudo dividir por ninguno de los de arriba, sí es primo
+        return true;
+      }
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        String par1 = request.getParameter("a");
+        int num = Integer.parseInt(par1);
+        int a=1;
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        try ( PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet ServletPrimos</title>");            
+            out.println("</head>");
+            out.println("<body>");        
+            while(list.size()<num){
+                if (esPrimo(a)) {
+                  out.println("<p>"+a+"</p>");
+                  a++;
+                  list.add(a);
+                }else{
+                    a++;
+                }
+            }            
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
